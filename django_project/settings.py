4f894464390fe19e27ae3fcd940616fe8acd70fc
revertiz <ignaspilipavicius@gmail.com>
2019-06-14 19:27:52 +0300

    ok

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,9 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#DJANGO BLOG VARS
-#export SECRET_KEY="e2e5d1788f622ced4fed86376b947c6a1e9bce654ab4fbd7"
-export DEBUG_VALUE=True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
