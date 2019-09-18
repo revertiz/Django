97159506b48d6f975d35dee0e830a4c2836d655f
revertiz <ignaspilipavicius@gmail.com>
2019-09-18 18:59:07 +0300

    test

--- users/models.py
+++ users/models.py
@@ -9,13 +9,33 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-#    def save(self, *args, **kwargs): #overwrite save method
-#        super().save(*args, **kwargs)
-#
-#        img = Image.open(self.image.path)
-#
-# #       if img.height > 300 or img.width > 300:
-#            output_size = (300,300)
-#            img.thumbnail(output_size)
-  #          img.save(self.image.path)
- #           
+    # def save(self, *args, **kwargs): #overwrite save method
+    #     super().save(*args, **kwargs)
+
+    #     img = Image.open(self.image.path)
+
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300,300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
+            
+# import io
+# from django.core.files.storage import default_storage as storage
+
+
+# def save(self, *args, **kwargs):
+#     super().save(*args, **kwargs)
+
+#     img_read = storage.open(self.image.name, 'r')
+#     img = Image.open(img_read)
+
+#     if img.height > 300 or img.width > 300:
+#         output_size = (300, 300)
+#         img.thumbnail(output_size)
+#         in_mem_file = io.BytesIO()
+#         img.save(in_mem_file, format='JPEG')
+#         img_write = storage.open(self.image.name, 'w+')
+#         img_write.write(in_mem_file.getvalue())
+#         img_write.close()
+
+#     img_read.close()
